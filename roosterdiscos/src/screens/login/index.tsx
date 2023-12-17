2a4a86716f97a68ab2fafe31af4ed7b3128ac803
unknown <johnatanpaes@gmail.com>
2023-12-17 00:49:13 -0300

    cadastro finalizado

--- roosterdiscos/src/screens/login/index.tsx
+++ roosterdiscos/src/screens/login/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import {StyleSheet,ImageBackground, View, Text} from 'react-native';
 //import background from './background.png';
+import {Button, Input} from '@rneui/base';
 
 
 
@@ -13,7 +14,20 @@ export function Loginscreen (props: LoginscreenProps) {
     return (
         <ImageBackground source={require(imgfundo)} style={styles.backG}>  
             <View style={styles.container}>
-                <Text style={styles.textAPP}> Será que vai?</Text>
+                <Input placeholder='Digite seu e-mail'
+                leftIcon={{name:'person', color:'white'}}
+                inputContainerStyle={styles.containerInput}/>
+                
+                <Input placeholder='Digite sua senha'
+                leftIcon={{name:'lock', color:'white'}}  secureTextEntry={true} 
+                inputContainerStyle={styles.containerInput}/>
+
+                <Button title={'Login'} buttonStyle={styles.containerButton}
+                />
+                
+                <Text style={styles.logar}>Não possui conta? Clique para se cadastrar</Text>
+
+                
                   
             </View>
         </ImageBackground>
@@ -23,10 +37,9 @@ export function Loginscreen (props: LoginscreenProps) {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
+        justifyContent:'flex-end' ,
         padding: 10,
-        alignItems: 'stretch'
-        
+        alignItems: 'center'
     },
     backG: {
         flex: 1,
@@ -36,6 +49,27 @@ const styles = StyleSheet.create({
     textAPP: {
         color: 'white',
         fontSize: 50,
+        textAlign: 'center'
+    },
+    containerInput:{
+        backgroundColor: 'rgba(255,255,255,0.7)',
+        borderRadius: 30,
+        padding: 5,
+        marginBottom: -15,
+        color: 'black'
+
+    },
+    containerButton:{
+        borderRadius: 30,
+        marginTop: 10,
+        marginHorizontal: 10,
+        padding: 15
+    },
+    logar:{
+        color: 'white',
+        fontSize: 20,
+        textDecorationLine: 'underline',
+        margin: 30,
         textAlign: 'center'
     }
 
