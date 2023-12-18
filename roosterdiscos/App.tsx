bc402c9d194993493326608b09aef8f8aecc8d8e
unknown <johnatanpaes@gmail.com>
2023-12-18 00:33:13 -0300

    cadastro finalizado

--- roosterdiscos/App.tsx
+++ roosterdiscos/App.tsx
@@ -2,11 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet,ImageBackground, Text, View } from 'react-native';
 import  {Loginscreen}  from './src/screens/login';
 import React from 'react';
-
+import { Cadastro } from './src/screens/cadastro/cadastro';
 
 export default function App() {
   return (
-     <Loginscreen/>
+     //<Loginscreen/>
+    <Cadastro/>
   );
 }
 
